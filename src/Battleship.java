38222d0d3e130b2394960f041838a89a736c8c4d
Andrea Savia <asavia3@gmail.com>
2020-06-15 01:17:13 +0100

    Print ocean with user's ships deployed

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Battleship {
@@ -26,26 +26,23 @@ public class Battleship {
         printOceanStrip();
 
         // Ask user to enter coordinate of 5 ships
-        Scanner input = new Scanner(System.in);
-        int[][] userShips = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
-
         int[] xy;
         boolean check;
 
         System.out.println("Deploy your ships:");
-        for (int i = 1; i <= 5; i++) {
-            xy = requestCoordinate(i);
-            check = checkRepeatedCoordinate(xy,userShips);
-            while (check) {
-                System.out.println("The coordinates entered for ship no. " + i + " have already been entered. Enter them again.");
-                xy = requestCoordinate(i);
-                check = checkRepeatedCoordinate(xy,userShips);
+        int j = 1;
+        do {
+            xy = requestCoordinate(j);
+            check = checkRepeatedCoordinate(xy,ocean);
+            if (check) {
+                System.out.println("The coordinates entered for ship no. " + j + " have already been entered. Enter them again.");
+            } else {
+                ocean[xy[0]][xy[1]] = 1;
+                j++;
             }
-            userShips[i-1] = xy;
-            ocean[xy[0]][xy[1]] = 1;
-        }
+        } while (check | j < 6);
 
-        // Print Ocean with user's ships
+        // Print ocean with user's ships
         printOceanStrip();
         for (int row = 0; row < ocean.length; row++) {
             System.out.print(row + "|");
@@ -62,14 +59,39 @@ public class Battleship {
         }
         printOceanStrip();
 
+        // Generating coordinates for the computer's ships
+        System.out.println("\nDeploying the computer's ships:");
 
+        Random rand = new Random();
+        int i = 0;
+        do {
+            xy[0] = rand.nextInt(9);
+            xy[1] = rand.nextInt(9);
+            check = checkRepeatedCoordinate(xy,ocean);
+            if (!check) {
+                ocean[xy[0]][xy[1]] = 2;
+                i++;
+            }
+        } while (check | i < 5);
 
-
-
-        //TODO Record the ship as 1 and printed out the Ocean Map using @
-
-        //TODO Deploy computer ships
-
+        // Print ocean with user's ships
+        printOceanStrip();
+        for (int row = 0; row < ocean.length; row++) {
+            System.out.print(row + "|");
+            for (int col = 0; col < ocean[row].length; col++) {
+                if (ocean[row][col] == 0) {
+                    System.out.print(" ");
+                } else if (ocean[row][col] == 1) {
+                    System.out.print("@");
+                } else if (ocean[row][col] == 2) {
+                    System.out.print("a");
+                } else {
+                    System.out.print(ocean[row][col]);
+                }
+            }
+            System.out.println("|" + row);
+        }
+        printOceanStrip();
 
     }
 
@@ -81,6 +103,7 @@ public class Battleship {
         }
         System.out.println(" "+" ");
     }
+
     public static int checkCoordinateOK(int a) {
         Scanner input = new Scanner(System.in);
         while (a < 0 | a > 9) {
@@ -89,6 +112,7 @@ public class Battleship {
         }
         return a;
     }
+
     public static int[] requestCoordinate(int i) {
 
         Scanner input = new Scanner(System.in);
@@ -101,18 +125,14 @@ public class Battleship {
         int y = input.nextInt();
         y = checkCoordinateOK(y);
 
-        int[] xy = {x,y};
-
-        return xy;
+        return new int[]{x,y};
     }
-    public static boolean checkRepeatedCoordinate(int[] xy, int[][] userShips) {
+
+    public static boolean checkRepeatedCoordinate(int[] xy, char[][] ocean) {
         boolean check = false;
-        for (int i = 0; i < userShips.length; i++) {
-            check = Arrays.equals(xy,userShips[i]);
-            if (check) {
-                break;
+            if (ocean[xy[0]][xy[1]]!=0) {
+                check = true;
             }
-        }
         return check;
     }
 }
